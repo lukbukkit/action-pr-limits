247fe534d579dc6c6f8943187d73a0447d6683c7
Lukas <git@lukasmx.de>
2026-04-09 15:59:20 +0200

    Fixes Jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts']
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  resolver: 'ts-jest-resolver',
+  testPathIgnorePatterns: ['/dist/', '/node_modules/'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        useESM: true
+      }
+    ]
+  },
+  verbose: true
 };
